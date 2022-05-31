5552648cca9a751b1f10daf7f7620a64ef72c555
Fernando Sanchez <sanchezfernandogabriel@outlook.com>
2022-05-30 21:06:12 -0300

    usando fetch

---
+++ stock.js
@@ -0,0 +1,32 @@
+const productos =
+[
+    {
+        "id": 1,
+        "nombre": "varita con pluma de fenix",
+        "desc": "varita preparada para magos que porten bondad, gentileza y un gran corazon",
+        "precio": 3200,
+        "img": "./public/images/harry.jpg",
+        "cantidad":4
+    },
+
+    {
+        "id": 2,
+        "nombre": "varita de basilisco",
+        "desc": "varita creada con huesos de un basilisco, se dice que solo un ser malvado de corazon puede manejarla",
+        "precio": 4500,
+        "img": "./public/images/volde.jpg",
+        "cantidad":4
+    },
+
+    {
+        "id": 3,
+        "nombre": "varita de sauco",
+        "desc": "la varita mas poderosa de todo el mundo magico , se dice que es una de las reliqueas de la muerte",
+        "precio":  6900,
+        "img": "./public/images/sauco.jpg",
+        "cantidad":4
+    }
+]
+
+export { productos };
+
